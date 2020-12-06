c71d896b8a5b89661206899a6e356b7e77c6e001
kevin-grylls <eilgwon88@gmail.com>
2020-12-06 11:48:08 +0900

    update code

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,13 @@
 module.exports = {
     verbose: true,
-    collectCoverageFrom: ['src/renderer/**', '!src/renderer/index.tsx', '!src/renderer/tests/**'],
+    collectCoverageFrom: [
+        'src/renderer/**',
+        '!src/renderer/index.tsx',
+        '!src/renderer/tests/**',
+        'src/main/**',
+        '!src/main/main.ts',
+        '!src/main/tests/**',
+    ],
     transform: { '^.+\\.ts?$': 'babel-jest' },
     testEnvironment: 'node',
     testRegex: '/tests/.*\\.(test|spec)?\\.(ts|tsx)$',
